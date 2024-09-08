c99f2d1df47274ea271d12effe94c5e46af08b36
Hoangg Chinh <hoanggchinh12@gmail.com>
2024-09-08 16:24:39 +0700

    create lib

---
+++ libFormula/chuVi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace libFormula
+{
+    public class chuVi
+    {
+        public int a, b, c;
+        public int p {  get; private set; }
+        public void tamGiac()
+        {
+            p = (a + b + c);
+        }
+    }
+}
