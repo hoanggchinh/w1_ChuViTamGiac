8ceaa8e02ccc597895fc720d29c9e3377c23547e
Toms <Toms@WIN-PRN7IADR449>
2024-09-08 14:06:59 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tinh chu vi tam giac");
+            int a, b, c;
+            Console.Write("a: "); a = int.Parse(Console.ReadLine());
+            Console.Write("b: "); b = int.Parse(Console.ReadLine());
+            Console.Write("c: "); c = int.Parse(Console.ReadLine());
+            int p = a + b + c;
+            Console.WriteLine("p = {0} + {1} + {2} = {3}", a, b, c, p);
+
+        }
+    }
+}
