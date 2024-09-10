6b0f1174727f6757c292ee6ca923284fc4415155
diamo.pc <diamo.pc@DESKTOP-5OVSU0B>
2024-09-10 08:08:01 +0700

    not pretty gud but i can't stay awake anymore

---
+++ WebV2/Tom.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    function send_api() {
+        var a = $('#txtA').val();
+        var b = $('#txtB').val();
+        var c = $('#txtC').val();
+
+        
+        if (isNaN(a) || a.trim() === "" || parseFloat(a) <= 0) {
+            $('#txtP').val('Check A\'s value again');
+            $('#txtA').focus();
+            return;
+        }
+        if (isNaN(b) || b.trim() === "" || parseFloat(b) <= 0) {
+            $('#txtP').val('Check B\'s value again');
+            $('#txtB').focus();
+            return;
+        }
+        if (isNaN(c) || c.trim() === "" || parseFloat(c) <= 0) {
+            $('#txtP').val('Check C\'s value again');
+            $('#txtC').focus();
+            return;
+        }
+
+        
+        a = parseFloat(a);
+        b = parseFloat(b);
+        c = parseFloat(c);
+
+        
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            $('#txtP').val('Can\'t make trigangle');
+            return;
+        }
+
+        
+        var perimeter = a + b + c;
+        $('#txtP').val(perimeter);
+    }
+
+    $("#button").click(function () {
+        send_api();
+    });
+});
